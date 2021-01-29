d55b04207b8b4340fed152bcb25685702e56badf
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-01-29 16:45:32 +0530

    Update code_17.cpp

--- code_17.cpp
+++ code_17.cpp
@@ -23,3 +23,29 @@ public:
         
     }
 };
+
+
+method 2
+    
+    class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k)
+    {
+        int twice_count=0;
+        map<int,int>m;
+        for(int i=0;i<n;i++)
+        {
+            m[arr[i]]++;
+        }
+        for (int i = 0; i < n; i++) {
+            twice_count += m[k - arr[i]];
+
+            // if (arr[i], arr[i]) pair satisfies the condition,
+            // then we need to ensure that the count is
+            // decreased by one such that the (arr[i], arr[i])
+            // pair is not considered
+            if (k - arr[i] == arr[i]) twice_count--;
+        }
+        return twice_count / 2;
+    }
+};
